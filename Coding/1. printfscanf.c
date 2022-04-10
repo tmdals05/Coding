384c0d669bd87703ad1e9dd0ec9f6f55b03d7082
daniel057988 <daniel057988@gmail.com>
2022-04-11 00:54:30 +0900

    4/11 AM 12:54

--- Coding/1. printfscanf.c
+++ Coding/1. printfscanf.c
@@ -2,51 +2,39 @@
 
 int main_printfscanf()
 {
-	/*
-	int age = 12;
-	printf("%d\n", age);
-	age = 13;
-	printf("%d\n", age);
-	float f = 46.5;
-	printf("%.2f\n", f);
-	double d = 4.428;
-	printf("%.2lf\n", d);
-	const int YEAR = 2000;
-	printf("태어난 년도 : %d\n", YEAR);
-	*/
+	//int age = 12;
+	//printf("%d\n", age);
+	//age = 13;
+	//printf("%d\n", age);
+	//float f = 46.5;
+	//printf("%.2f\n", f);
+	//double d = 4.428;
+	//printf("%.2lf\n", d);
+	//const int YEAR = 2000;
+	//printf("태어난 년도 : %d\n", YEAR);
 
-	/*
-	int add = 3 + 7;
-	printf("3 + 7 = %d\n", add);
-	printf("%d * %d = %d\n", 30, 79, 30 * 79);
-	*/
+	//int add = 3 + 7;
+	//printf("3 + 7 = %d\n", add);
+	//printf("%d * %d = %d\n", 30, 79, 30 * 79);
 
-	/*
-	int input;
-	printf("값을 입력하세요 : ");
-	scanf_s("%d", &input);
-	printf("입력값 : %d\n", input);
-	*/
+	//int input;
+	//printf("값을 입력하세요 : ");
+	//scanf_s("%d", &input);
+	//printf("입력값 : %d\n", input);
 
-	/*
-	int one, two, three;
-	printf("3개의 정수를 입력하세요 : ");
-	scanf_s("%d %d %d", &one, &two, &three);
-	printf("첫번째 값 : %d\n", one);
-	printf("두번째 값 : %d\n", two);
-	printf("세번째 값 : %d\n", three);
-	*/
+	//int one, two, three;
+	//printf("3개의 정수를 입력하세요 : ");
+	//scanf_s("%d %d %d", &one, &two, &three);
+	//printf("첫번째 값 : %d\n", one);
+	//printf("두번째 값 : %d\n", two);
+	//printf("세번째 값 : %d\n", three);
 
-	/*
-	char c = 'A';
-	printf("%c\n", c);
-	*/
+	//char c = 'A';
+	//printf("%c\n", c);
 
-	/*
-	char str[256];
-	scanf_s("%s", str, sizeof(str));
-	printf("%s\n", str);
-	*/
+	//char str[256];
+	//scanf_s("%s", str, sizeof(str));
+	//printf("%s\n", str);
 
 	char name[256];
 	printf("이름이 뭐예요?\n");
