f2324403e5939cf2c0757b0a402f07a8b51347a9
tmdals05 <94149010+tmdals05@users.noreply.github.com>
2022-06-02 10:31:38 +0900

    Update login.c
    finish

--- Coding/login.c
+++ Coding/login.c
@@ -57,24 +57,54 @@ void SignUp()
 {
 	char tempID[50];
 	char tempPW[50];
+	char tempPW_check[50];
 	system("cls");
 	printf("%s", "아이디를 입력하세요 : ");
 	scanf("%s", tempID);
 	printf("%s", "비밀번호를 입력하세요 : ");
 	scanf("%s", tempPW);
-	ID_OverlapCheck(tempID);
-	strcpy(num[count].ID, tempID);
-	strcpy(num[count].PW, tempPW);
-	printf("회원가입이 되었습니다!\n");
-	printf("%s", "메뉴로 돌아가려면 아무키나 누르세요..");
-	while (1)
+	printf("%s", "비밀번호를 다시 입력하세요 : ");
+	scanf("%s", tempPW_check);
+	if (strcmp(tempPW, tempPW_check) != 0)
 	{
-		if (_kbhit())
+		printf("비밀번호가 다릅니다\n");
+		printf("%s", "메뉴로 돌아가려면 아무키나 누르세요..");
+		while (1)
 		{
-			break;
+			if (_kbhit())
+			{
+				return 0;
+			}
 		}
 	}
-	count++;
+	if (ID_OverlapCheck(tempID) == 1)
+	{
+		printf("중복된 아이디가 있습니다\n");
+		printf("%s", "메뉴로 돌아가려면 아무키나 누르세요..");
+		while (1)
+		{
+			if (_kbhit())
+			{
+				return 0;
+			}
+		}
+	}
+	else
+	{
+		strcpy(num[count].ID, tempID);
+		strcpy(num[count].PW, tempPW);
+		printf("회원가입이 되었습니다!\n");
+		printf("%s", "메뉴로 돌아가려면 아무키나 누르세요..");
+		while (1)
+		{
+			if (_kbhit())
+			{
+				break;
+			}
+		}
+		count++;
+	}
+
 }
 
 void SignIn()
@@ -100,17 +130,13 @@ void SignIn()
 				}
 			}
 		}
-		else
+	}
+	printf("아이디와 비밀번호를 다시한번 확인해주세요\n");
+	printf("%s", "메뉴로 돌아가려면 아무키나 누르세요..");
+	while (1)
+	{
+		if (_kbhit())
 		{
-			printf("아이디와 비밀번호를 다시한번 확인해주세요\n");
-			printf("%s", "메뉴로 돌아가려면 아무키나 누르세요..");
-			while (1)
-			{
-				if (_kbhit())
-				{
-					break;
-				}
-			}
 			break;
 		}
 	}
@@ -136,14 +162,14 @@ void findPW()
 				}
 			}
 		}
-		printf("아이디가 존재하지 않습니다\n");
-		printf("%s", "메뉴로 돌아가려면 아무키나 누르세요..");
-		while (1)
+	}
+	printf("아이디가 존재하지 않습니다\n");
+	printf("%s", "메뉴로 돌아가려면 아무키나 누르세요..");
+	while (1)
+	{
+		if (_kbhit())
 		{
-			if (_kbhit())
-			{
-				break;
-			}
+			break;
 		}
 	}
 }
@@ -154,18 +180,10 @@ int ID_OverlapCheck(char ID[50])
 	{
 		if (strcmp(ID, num[i].ID) == 0)
 		{
-			printf("중복된 아이디가 있습니다\n");
-			printf("%s", "메뉴로 돌아가려면 아무키나 누르세요..");
-			while (1)
-			{
-				if (_kbhit())
-				{
-					break;
-				}
-			}
-			break;
+			return 1;
 		}
 	}
+	return 0;
 }
 
 void CursorView(char tf)
